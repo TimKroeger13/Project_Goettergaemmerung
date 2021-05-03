492e6dcdd2532ef07c75a197739bcf8290a25da4
Tim <t.kroeger13@googlemail.com>
2021-05-03 15:18:58 +0200

    Refactored solution structure

---
+++ Goetterdaemmerung.Unittests/Components/CardInformationGetter/OdsCardInformationTests.cs
@@ -0,0 +1,35 @@
+﻿using NSubstitute;
+using Project_Goettergaemmerung.Components.CardInformationGetter;
+using System;
+using Xunit;
+
+namespace Unittests.Components.CardInformationGetter
+{
+    public class OdsCardInformationTests
+    {
+
+
+        public OdsCardInformationTests()
+        {
+
+        }
+
+        private OdsCardInformation CreateOdsCardInformation()
+        {
+            return new OdsCardInformation();
+        }
+
+        [Fact]
+        public void GetCardInformation_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var odsCardInformation = this.CreateOdsCardInformation();
+
+            // Act
+            var result = odsCardInformation.GetCardInformation();
+
+            // Assert
+            Assert.True(false);
+        }
+    }
+}
---
+++ Goetterdaemmerung.Unittests/Components/CreatePictureTests.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using Project_Goettergaemmerung.Components;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using Xunit;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Linq;
+using Project_Goettergaemmerung.ExtensionMethods;
+
+namespace Unittests.Components
+{
+    public class CreatePictureTests
+    {
+        private CreatePicture CreateCreatePicture()
+        {
+            return new CreatePicture();
+        }
+
+        [Fact]
+        public void BledingMultiply_StateUnderTest_ExpectedBehavior()
+        {
+            var createPicture = CreateCreatePicture();
+            Bitmap bitmap1 = TestResources.TestImage;
+            Bitmap bitmap2 = TestResources.TestImage;
+            var sw = new Stopwatch();
+
+            sw.Start();
+            using var result = createPicture.BlendingMultiply(bitmap1, bitmap2);
+            sw.Stop();
+
+            var resultBytes = result.GetArgbBytes();
+            var testResultBytes = TestResources.TestResult.GetArgbBytes();
+            sw.ElapsedMilliseconds.Should().BeLessThan(500);
+            GetHash(resultBytes).Should().Be(GetHash(testResultBytes));
+        }
+
+        private static string GetHash(byte[] data)
+        {
+            using var sha = System.Security.Cryptography.SHA1.Create();
+            return string.Concat(sha.ComputeHash(data).Select(d => d.ToString("X2")));
+        }
+    }
+}
